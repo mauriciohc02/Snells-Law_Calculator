ab7bc6cd3c51bae4fab4336dd9385529e0872521
mauriciohc02 <72422185+mauriciohc02@users.noreply.github.com>
2022-12-30 21:30:01 -0600

    Update Dockerfile and run.sh
    Changes:
    - Dockerfile has been changed to make a lighter image
    - run.sh has been improved for preventing errors

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,23 @@
 FROM python:3.10-slim
 
+COPY . /home/Snells-Law_Calculator/
 WORKDIR /home/Snells-Law_Calculator
 
-COPY . /home/Snells-Law_Calculator/
-
 RUN apt update && \
-    apt install python3-pyqt5 -y && \
-    pip3 install -r requirements.txt
+    apt install -y --no-install-recommends \
+        libgl1 \
+        libfontconfig1 \
+        shared-mime-info \
+        libdbus-1-3 \
+        libxcb-image0 \
+        libxcb-keysyms1 \
+        libxcb-shape0 \
+        libxcb-randr0 \
+        libxcb-xinerama0 \
+        libxcb-icccm4 \
+        libxkbcommon-x11-0 \
+        libxcb-render-util0 && \
+    rm -rf /var/lib/apt/lists/* && \
+    pip install --no-cache-dir -r requirements.txt
 
 CMD [ "python3", "main.py" ]
--- run.sh
+++ run.sh
@@ -1,10 +1,61 @@
 #!/bin/bash
 
-xhost +local:* &>> output.txt &&
-docker-compose up &>> output.txt |
-docker compose up &>> output.txt &&
-docker-compose down &>> output.txt |
-docker compose down &>> output.txt &&
-xhost -local:* &>> output.txt
 
-rm output.txt
+CONTAINER_NAME="Snells-Law_Calculator"
+IMAGE_NAME="mauriciohc/snells-law:latest"
+DOCKER_RUN_PARAMS="-e "DISPLAY" --network host -v $(pwd)/plots:/home/Snells-Law_Calculator/plots:rw --name $CONTAINER_NAME $IMAGE_NAME"
+
+
+function is_docker_install() {
+    # Verify if the whole script can be run by checking if Docker is installed
+    if ! [[ $(docker version 2>/dev/null) ]]
+    then
+        echo "Install Docker in order to run this script successfully"
+        exit 1
+    fi
+}
+
+
+function is_docker_compose_install() {
+    # Verify if some of these commands do exist
+    if [[ $(docker compose version 2>/dev/null) ]]
+    then
+        echo "Running $CONTAINER_NAME with Docker Compose"
+        return 1
+    elif [[ $(docker-compose --version 2>/dev/null) ]]
+    then
+        echo "Running $CONTAINER_NAME with Docker-Compose"
+        return 2
+    else
+        echo "Running $CONTAINER_NAME just with Docker"
+        return 3
+    fi
+}
+
+
+function run_container() {
+    # Based on is_docker_compose_install(), choose which commands will use
+    is_docker_compose_install
+    case "$?" in
+        1)
+            docker compose up &>/dev/null
+            docker compose down &>/dev/null
+            ;;
+        2)
+            docker-compose up &>/dev/null
+            docker-compose down &>/dev/null
+            ;;
+        3)
+            docker run --rm $DOCKER_RUN_PARAMS &>/dev/null
+            ;;
+    esac
+}
+
+
+is_docker_install
+xhost +local:* &>/dev/null
+run_container
+echo "$CONTAINER_NAME Container has been stopped and removed"
+xhost -local:* &>/dev/null
+
+exit 0
